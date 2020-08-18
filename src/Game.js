96b25d7a28244e49906a250d5cafca400e3184b1
Katy St Sauveur <katyrogowski@yahoo.com>
2020-08-18 11:55:56 -0600

    Wrote implementation code for Game class

--- src/Game.js
+++ src/Game.js
@@ -3,7 +3,22 @@ const prototypeQuestions = data.prototypeData;
 const util = require('./util');
 
 class Game {
-  constructor() {}
+  constructor() {
+    this.round = null;
+  }
+
+  start() {
+  let cards = [];
+  prototypeQuestions.forEach(question => {
+    let card = new Card(question['id'], question['question'], question['answers'], question['correctAnswer'])
+    cards.push(card);
+  });
+  const deck = new Deck(cards);
+  this.round = new Round(deck);
+  this.printMessage(deck, this.round);
+  this.printQuestion(this.round);
+}
+
 
   printMessage(deck, round) {
       console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
