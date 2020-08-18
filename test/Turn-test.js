5731e8cb6a123f8eec856a006b5bcd13e85657b2
Katy St Sauveur <katyrogowski@yahoo.com>
2020-08-17 19:21:28 -0700

    Modify test for Turn class

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -7,11 +7,11 @@ const Turn = require('../src/Turn');
 describe('Turn', function() {
   let card, guess1, guess2, turn1, turn2;
   beforeEach(function() {
-    const card = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-    const guess1 = 'A users guess';
-    const guess2 = 'sea otter';
-    const turn1 = new Turn(guess1, card);
-    const turn2 = new Turn(guess2, card)
+    card = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    guess1 = 'A users guess';
+    guess2 = 'sea otter';
+    turn1 = new Turn(guess1, card);
+    turn2 = new Turn(guess2, card)
 
   });
 
@@ -20,15 +20,17 @@ describe('Turn', function() {
   });
 
   it('should be an instance of Turn', function() {
-    expect(turn).to.be.an.instanceof(Turn);
+    expect(turn1).to.be.an.instanceof(Turn);
   });
 
   it('should return a users guess', function() {
     expect(guess1).to.equal(turn1.returnGuess());
+    expect(guess2).to.equal(turn2.returnGuess());
   });
 
   it('should return a flashcard', function() {
     expect(card).to.equal(turn1.returnCard());
+    expect(card).to.equal(turn2.returnCard());
   });
 
   it('should evaluate a users guess', function() {
