2478e47e08562d206b45c9f8afd72e6ed176f90f
Katy St Sauveur <katyrogowski@yahoo.com>
2020-08-18 11:48:47 -0600

    Wrote tests for Game class

---
+++ test/Game-test.js
@@ -0,0 +1,23 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Game = require('../src/Game');
+
+describe('Game', function() {
+  let game;
+  beforeEach(function() {
+    game = new Game();
+  })
+
+  it('should be a function', function() {
+    expect(Game).to.be.a('function');
+  });
+
+  it('should be an instance of Game', function() {
+    expect(game).to.be.an.instanceof(Game);
+  });
+
+  it('should make sure the current round is set to null', function() {
+    expect(game.round).to.equal(null);
+  });
+});
