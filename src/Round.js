cc52ac7b13d5b775ad2a03bc29cd9535c820a10d
Katy St Sauveur <katyrogowski@yahoo.com>
2020-08-18 11:40:15 -0600

    Wrote implementation code for Round class

--- src/Round.js
+++ src/Round.js
@@ -1,69 +1,31 @@
-// Your Round class will be the object that takes in responses and records these guesses (as well as if they are correct or incorrect). The currentCard should be the first Card in the Deck (the array of Cards) at the start of the Round
-//
-// Your Round class should meet the following requirements:
-// returnCurrentCard: method that returns the current card being played
-// takeTurn: method that updates turns count, evaluates guesses, gives feedback, and stores ids of incorrect guesses
-//
-// When a guess is made, a new Turn instance is created.
-// The turns count is updated, regardless of whether the guess is correct or incorrect
-// The next card becomes current card
-// Guess is evaluated/recorded. Incorrect guesses will be stored (via the id) in an array of incorrectGuesses
-// Feedback is returned regarding whether the guess is incorrect or correct
-// calculatePercentCorrect: method that calculates and returns the percentage of correct guesses
-// endRound: method that prints the following to the console: ‘** Round over! ** You answered <>% of the questions correctly!’
-// For example:
-//
-// const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-// const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
-// const card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
-//
-// const deck = new Deck([card1, card2, card3]);
-//
-// const round = new Round(deck);
-//
-// round.deck;   // => [card1, card2, card3];
-//
-// round.returnCurrentCard(); // => { id: 1,
-//                            //      question: 'What is Robbie\'s favorite animal',
-//                            //      answers: ['sea otter', 'pug', 'capybara'],
-//                            //      correctAnswer: 'sea otter'
-//                            //    }
-//
-// round.turns; // => 0
-//
-// round.incorrectGuesses;     // => []
-//
-// round.takeTurn('sea otter'); // => 'correct!'
-//
-// round.takeTurn('spleen');   // => 'incorrect!'
-//
-// round.turns; // => 2
-//
-// round.incorrectGuesses;     // => [14]
-//
-// round.returnCurrentCard();    // => { id: 12,
-//             	              //      question: 'What is Travis\'s favorite stress reliever?',
-//             	              //      answers: ['listening to music', 'watching Netflix', 'playing with bubble wrap'],
-//             	              //      correctAnswer: 'playing with bubble wrap'
-//             	              //    }
-//
-// round.calculatePercentCorrect(); // => 50
+const Turn = require('./Turn');
 
-class Round() {
-  constructor() {
-
+class Round {
+  constructor(deck) {
+    this.deckCards = deck.cards;
+    this.turns = 0;
+    this.incorrectGuesses = [];
   }
   returnCurrentCard() {
-
+    return this.deckCards[this.turns]
   }
-  takeTurn() {
-
+  takeTurn(guess) {
+    const card = this.deckCards[this.turns];
+    this.turns ++;
+    const turn = new Turn(guess, card);
+    if (!turn.evaluateGuess()) {
+      this.incorrectGuesses.push(card.id);
+    }
+    return turn.giveFeedback();
   }
+
   calculatePercentCorrect() {
-
+    return Math.floor(((this.turns - this.incorrectGuesses.length) / this.turns) * 100);
   }
+
   endRound() {
-    
+    console.log(`** Round over! ** You answered ${this.calculatePercentCorrect()}% of the questions correctly!`);
+    return process.exit();
   }
 }
 
