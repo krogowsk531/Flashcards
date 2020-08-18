0e6551af2ff6fab9b785df150adbb8034f89996f
Katy St Sauveur <katyrogowski@yahoo.com>
2020-08-17 17:14:56 -0700

    Build basic skeleton for Turn class

---
+++ src/Turn.js
@@ -0,0 +1,37 @@
+// const card = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+// const turn = new Turn('pug', card);
+//
+// turn.returnGuess();    // => 'pug'
+//
+//
+// turn.returnCard();     // => { id: 1,
+//                        //      question: 'What is Robbie\'s favorite animal',
+//                        //      answers: ['sea otter', 'pug', 'capybara'],
+//                        //      correctAnswer: 'sea otter'
+//                        //    }
+//
+//
+// turn.evaluateGuess();  // => false
+//
+//
+// turn.giveFeedback();   // => incorrect!
+
+class Turn {
+  constructor() {
+
+  }
+  returnGuess() {
+
+  }
+  returnCard() {
+
+  }
+  evaluateGuess() {
+
+  }
+  giveFeedback() {
+    
+  }
+}
+
+module.exports = Turn;
